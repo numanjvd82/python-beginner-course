1ad87036b32ab767998b5475131191f47fcd376f
Numan Javed <nomanmani62@gmail.com>
2021-10-21 15:57:28 +0500

    nested loops

---
+++ nestedLoops&2Dlists.py
@@ -0,0 +1,16 @@
+number_grid = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [0]
+]
+
+
+def print_grid(grid):
+    for row in grid:
+        for number in row:
+            print(number)
+
+
+print_grid(number_grid)
+print(number_grid)
