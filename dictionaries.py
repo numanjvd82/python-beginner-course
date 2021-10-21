fc1d32e16008d2ff378f5fee8f741903156b1a23
Numan Javed <nomanmani62@gmail.com>
2021-10-21 15:08:32 +0500

    dictionaries and while loop

---
+++ dictionaries.py
@@ -0,0 +1,20 @@
+# Dictionaries are key value pairs.
+
+firstDictionary = {
+    "jan": "January",
+    "feb": "February",
+    "mar": "March",
+    "apr": "April",
+    "may": "May",
+    "jun": "June",
+    "jul": "July",
+    "aug": "August",
+    "sep": "September",
+    "oct": "October",
+    "nov": "November",
+    "dec": "December"
+}
+
+
+print(firstDictionary["jan"])
+print(firstDictionary.get("lul", "Not a valid key"))
---
+++ whileLoop.py
@@ -0,0 +1,9 @@
+# While loop
+
+i = 1
+
+while i <= 10:
+    print(i)
+    i += 1
+
+print('Done with Loop')
